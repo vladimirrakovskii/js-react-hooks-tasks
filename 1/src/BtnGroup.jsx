2745767a72daf135ce8206ecdd4e6a52c96341a4
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:36:27 +0300

    Update BtnGroup.jsx

--- 1/src/BtnGroup.jsx
+++ 1/src/BtnGroup.jsx
@@ -3,7 +3,28 @@ import React, { useState } from 'react';
 
 const BtnGroup = () => {
   // BEGIN (write your solution here)
-
+const [isLeftPressed, handleLeftClick] = React.useState(false);
+    const [isRightPressed, handleRightClick] = React.useState(false);
+    const leftButtonClasses = cn('btn', 'btn-secondary', 'left', {
+        "active": isLeftPressed
+    });
+    const rightButtonClasses = cn('btn', 'btn-secondary', 'right', {
+        "active": isRightPressed
+    });
+    return (
+        <div className="btn-group" role="group">
+            <button type="button" className={ leftButtonClasses } onClick={() => {
+                handleLeftClick(true);
+                handleRightClick(false);
+            }}
+            >Left</button>
+            <button type="button" className={ rightButtonClasses } onClick={() => {
+                handleLeftClick(false);
+                handleRightClick(true);
+            }}
+            >Right</button>
+        </div>
+    )
   // END
 };
 
