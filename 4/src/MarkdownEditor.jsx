f31d627b687154faed6f0800b708d6753c2f0fec
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:41:58 +0300

    Update MarkdownEditor.jsx

--- 4/src/MarkdownEditor.jsx
+++ 4/src/MarkdownEditor.jsx
@@ -3,7 +3,28 @@ import Editor from '@toast-ui/editor';
 
 const MarkdownEditor = ({ onContentChange }) => {
   // BEGIN (write your solution here)
+  const editorRef = useRef(null);
 
+    React.useEffect(() => {
+        const editor = new Editor({
+            el: editorRef.current,
+            hideModeSwitch: true,
+        });
+
+        editor.addHook("change", () => {
+            const content = editor.getMarkdown();
+            if (onContentChange) {
+                onContentChange(content);
+            }
+        });
+        return () => {
+            editor.removeHook("change");
+            editor.destroy();
+        };
+    }, [onContentChange]);
+    return (
+        <div ref={editorRef} />
+    )
   // END
 };
 
