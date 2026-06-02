726b664095e591c7782d0ad0ecc7aea11224e825
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:45:29 +0300

    Update App.jsx

--- 6/src/App.jsx
+++ 6/src/App.jsx
@@ -2,7 +2,71 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 // BEGIN (write your solution here)
+const Factorial = ({ value }) => {
+    const factorialFunc = (number) => {
+        if (number <= 0) return 1;
+        return number * factorialFunc(number - 1);
+    };
 
+    const result = factorialFunc(value);
+    return (
+        <div>{`Factorial of ${value} is ${result}`}</div>
+    );
+};
+
+const App = () => {
+    const { t, i18n } = useTranslation();
+    const [value, setValue] = useState(0);
+
+    const handleLangSwitch = (e) => {
+        const lang = e.target.dataset.testid;
+        i18n.changeLanguage(lang);
+    };
+
+    const getClassName = (currLang) => {
+        const className = i18n.language === currLang ? 'btn btn-primary' : 'btn btn-outline-primary';
+        return className;
+    };
+
+    return (
+        <div className="App">
+            <div className="btn-group mb-3" role="group">
+                <button
+                    type="button"
+                    data-testid="en"
+                    className={getClassName('en')}
+                    onClick={handleLangSwitch}
+                >
+                    {t('languages.en')}
+                </button>
+                <button
+                    type="button"
+                    data-testid="ru"
+                    className={getClassName('ru')}
+                    onClick={handleLangSwitch}
+                >
+                    {t('languages.ru')}
+                </button>
+            </div>
+            <br />
+            <div className="btn-group mb-3" role="group">
+                <button type="button" className="btn btn-outline-primary" onClick={() => setValue(1)}>
+                    {t('factorial')} 1
+                </button>
+                <button type="button" className="btn btn-outline-primary" onClick={() => setValue(5)}>
+                    {t('factorial')} 5
+                </button>
+                <button type="button" className="btn btn-outline-primary" onClick={() => setValue(10)}>
+                    {t('factorial')} 10
+                </button>
+                <button type="button" className="btn btn-outline-primary" onClick={() => setValue(20)}>
+                    {t('factorial')} 20
+                </button>
+            </div>
+            <Factorial value={value} />
+        </div>
+    );
+};
 // END
 
 export default App;
